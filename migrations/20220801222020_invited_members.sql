02c9e939fc5d2d33293bb5e520bf1b586f17a688
Erik Tesar <erik@erik-tesar.com>
2022-08-02 00:49:23 +0200

    feat: add table for invited members

---
+++ migrations/20220801222020_invited_members.sql
@@ -0,0 +1,12 @@
+-- `user`: The user who used the `invite`
+-- `inviter`: The user who created the `invite`
+-- `invite`: The code of the invite (the part after `discord.gg/`)
+-- `guild`: The guild this invite belongs to
+-- `used_at`: time the user joined the guild
+CREATE TABLE invited_members(
+    "user" TEXT PRIMARY KEY NOT NULL,
+    "inviter" TEXT NOT NULL,
+    "invite" TEXT NOT NULL,
+    "guild" TEXT NOT NULL,
+    "used_at" TIMESTAMPTZ NOT NULL DEFAULT now()
+)
